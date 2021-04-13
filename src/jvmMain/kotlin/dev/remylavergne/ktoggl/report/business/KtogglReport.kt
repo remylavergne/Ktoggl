3557dd093e5dfb18a46a54edddac386fc8c61b8f
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-04-13 19:41:04 +0200

    refactor ktoggl report client + detail endpoint

--- src/jvmMain/kotlin/dev/remylavergne/ktoggl/report/business/KtogglReport.kt
+++ src/jvmMain/kotlin/dev/remylavergne/ktoggl/report/business/KtogglReport.kt
@@ -2,14 +2,9 @@ package dev.remylavergne.ktoggl.report.business
 
 import dev.remylavergne.ktoggl.BASE_URL
 import dev.remylavergne.ktoggl.duplicate
-import dev.remylavergne.ktoggl.report.service.ApiResult
-import dev.remylavergne.ktoggl.report.KtogglReportApi
-import dev.remylavergne.ktoggl.report.common.WeeklyParams
-import dev.remylavergne.ktoggl.report.common.Param
-import dev.remylavergne.ktoggl.report.common.ParamCalculate
-import dev.remylavergne.ktoggl.report.common.ParamGrouping
-import dev.remylavergne.ktoggl.report.common.Params
+import dev.remylavergne.ktoggl.report.common.*
 import dev.remylavergne.ktoggl.report.models.*
+import dev.remylavergne.ktoggl.report.service.ApiResult
 import dev.remylavergne.ktoggl.toQueryParams
 import io.ktor.client.*
 import io.ktor.client.request.*
@@ -23,50 +18,74 @@ typealias WeeklyUsersEarningsResult = BaseEarnings<List<WeeklyUsersEarnings>>
 
 data class KtogglReport(
     private val account: ApiCredentials,
-) : KtogglReportApi {
+) {
 
-    override suspend fun weeklyProjectsTime(block: Params.() -> Unit): ApiResult<WeeklyProjectsTimeResult> {
+    suspend fun weeklyProjectsTime(block: Params.() -> Unit): ApiResult<WeeklyProjectsTimeResult> {
         val params = Params().apply(block).get()
-        return this.getWeekly(*params, WeeklyParams.grouping(), WeeklyParams.calculate())
+        return this.makeApiCall(
+            *params, WeeklyParams.grouping(), WeeklyParams.calculate(),
+            endpoint = Endpoint.WEEKLY,
+        )
     }
 
-    override suspend fun weeklyProjectsEarnings(block: Params.() -> Unit): ApiResult<WeeklyProjectsEarningsResult> {
+    suspend fun weeklyProjectsEarnings(block: Params.() -> Unit): ApiResult<WeeklyProjectsEarningsResult> {
         val params = Params().apply(block).get()
-        return this.getWeekly(
+        return this.makeApiCall(
             *params,
             WeeklyParams.grouping(),
-            WeeklyParams.calculate(ParamCalculate.Calculate.EARNINGS)
+            WeeklyParams.calculate(ParamCalculate.Calculate.EARNINGS),
+            endpoint = Endpoint.WEEKLY,
         )
     }
 
-    override suspend fun weeklyUsersTime(block: Params.() -> Unit): ApiResult<WeeklyUsersTimeResult> {
+    suspend fun weeklyUsersTime(block: Params.() -> Unit): ApiResult<WeeklyUsersTimeResult> {
         val params = Params().apply(block).get()
-        return this.getWeekly(
+        return this.makeApiCall(
             *params,
             WeeklyParams.grouping(ParamGrouping.Grouping.USERS),
-            WeeklyParams.calculate()
+            WeeklyParams.calculate(),
+            endpoint = Endpoint.WEEKLY,
         )
     }
 
-    override suspend fun weeklyUsersEarnings(block: Params.() -> Unit): ApiResult<WeeklyUsersEarningsResult> {
+    suspend fun weeklyUsersEarnings(block: Params.() -> Unit): ApiResult<WeeklyUsersEarningsResult> {
         val params = Params().apply(block).get()
-        return this.getWeekly(
+        return this.makeApiCall(
             *params,
             WeeklyParams.grouping(ParamGrouping.Grouping.USERS),
-            WeeklyParams.calculate(ParamCalculate.Calculate.EARNINGS)
+            WeeklyParams.calculate(ParamCalculate.Calculate.EARNINGS),
+            endpoint = Endpoint.WEEKLY,
         )
     }
 
-    private suspend inline fun <reified T> getWeekly(vararg params: Param<*> = arrayOf()): ApiResult<T> {
+    suspend fun details(page: Int = 1, block: Params.() -> Unit): ApiResult<BaseDetailed> {
+        if (page <= 0) {
+            throw Exception("Minimum page must be 1")
+        }
+        val params = Params().apply(block).get()
+        val pageParam: Param<Int> = DetailedParams.page(page)
+        println(page)
+        return this.makeApiCall(*params, pageParam, endpoint = Endpoint.DETAILED)
+    }
+
+    private suspend inline fun <reified T> makeApiCall(
+        vararg params: Param<*> = arrayOf(),
+        endpoint: Endpoint
+    ): ApiResult<T> {
         if (params.duplicate()) {
-            return ApiResult.Error(message = "Varargs contains duplicate values")
+            return ApiResult.Error(message = "Some query params are duplicated")
         }
 
         val queryParams: String = arrayOf(*params).toQueryParams()
 
         val result: T =
-            account.httpClient.use { client: HttpClient -> client.get<T>("$BASE_URL/weekly$queryParams") }
+            account.httpClient.use { client: HttpClient -> client.get<T>("$BASE_URL${endpoint.v}$queryParams") }
 
         return ApiResult.Success(result)
     }
+}
+
+enum class Endpoint(val v: String) {
+    WEEKLY("/weekly"),
+    DETAILED("/details"),
 }
