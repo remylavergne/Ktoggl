8fd3e22221fda63ccdaa9112050b6f78398a33c1
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-04-17 10:49:03 +0200

    functions to consume summary api

--- src/jvmMain/kotlin/dev/remylavergne/ktoggl/report/business/KtogglReport.kt
+++ src/jvmMain/kotlin/dev/remylavergne/ktoggl/report/business/KtogglReport.kt
@@ -1,8 +1,10 @@
 package dev.remylavergne.ktoggl.report.business
 
-import dev.remylavergne.ktoggl.BASE_URL
+import dev.remylavergne.ktoggl.REPORT_BASE_URL
 import dev.remylavergne.ktoggl.duplicate
 import dev.remylavergne.ktoggl.report.common.*
+import dev.remylavergne.ktoggl.report.common.SummaryParams.grouping
+import dev.remylavergne.ktoggl.report.common.SummaryParams.subgrouping
 import dev.remylavergne.ktoggl.report.models.*
 import dev.remylavergne.ktoggl.report.service.ApiResult
 import dev.remylavergne.ktoggl.toQueryParams
@@ -64,9 +66,134 @@ data class KtogglReport(
         }
         val params = Params().apply(block).get()
         val pageParam: Param<Int> = DetailedParams.page(page)
-        println(page)
         return this.makeApiCall(*params, pageParam, endpoint = Endpoint.DETAILED)
     }
+
+    /**
+     * Summary Projects
+     */
+
+    suspend fun summaryProjectsTimeEntries(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryTimeEntry>> { // TODO: Alias ?
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(),
+            subgrouping(),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryProjectsTasks(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryTask>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(),
+            subgrouping(ParamSummarySubgrouping.By.TASKS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryProjectsUsers(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryUser>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(),
+            subgrouping(ParamSummarySubgrouping.By.USERS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    /**
+     * Summary Clients
+     */
+
+    suspend fun summaryClientsTimeEntries(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryTimeEntry>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.CLIENTS),
+            subgrouping(ParamSummarySubgrouping.By.TIME_ENTRIES),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryClientsTasks(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryTask>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.CLIENTS),
+            subgrouping(ParamSummarySubgrouping.By.TASKS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryClientsProjects(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryProject>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.CLIENTS),
+            subgrouping(ParamSummarySubgrouping.By.PROJECTS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryClientsUsers(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryUser>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.CLIENTS),
+            subgrouping(ParamSummarySubgrouping.By.USERS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    /**
+     * Summary Users
+     */
+
+    suspend fun summaryUsersTimeEntries(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryTimeEntry>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.USERS),
+            subgrouping(ParamSummarySubgrouping.By.TIME_ENTRIES),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryUsersTasks(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryTask>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.USERS),
+            subgrouping(ParamSummarySubgrouping.By.TASKS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryUsersProjects(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryProject>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.USERS),
+            subgrouping(ParamSummarySubgrouping.By.PROJECTS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    suspend fun summaryUsersClients(block: Params.() -> Unit): ApiResult<BaseSummaryProjects<SummaryProject, SummaryClient>> {
+        val params = Params().apply(block).get()
+        return this.makeApiCall(
+            *params,
+            grouping(ParamSummaryGrouping.By.USERS),
+            subgrouping(ParamSummarySubgrouping.By.CLIENTS),
+            endpoint = Endpoint.SUMMARY,
+        )
+    }
+
+    /**
+     * Private
+     */
 
     private suspend inline fun <reified T> makeApiCall(
         vararg params: Param<*> = arrayOf(),
@@ -79,13 +206,8 @@ data class KtogglReport(
         val queryParams: String = arrayOf(*params).toQueryParams()
 
         val result: T =
-            account.httpClient.use { client: HttpClient -> client.get<T>("$BASE_URL${endpoint.v}$queryParams") }
+            account.httpClient.use { client: HttpClient -> client.get<T>("$REPORT_BASE_URL${endpoint.v}$queryParams") }
 
         return ApiResult.Success(result)
     }
-}
-
-enum class Endpoint(val v: String) {
-    WEEKLY("/weekly"),
-    DETAILED("/details"),
 }
