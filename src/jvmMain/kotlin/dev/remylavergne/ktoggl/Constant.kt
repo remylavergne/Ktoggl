b13e8bdd01a277e0f7611e605bc67aaa55116d1e
Rémy LAVERGNE <r.lavergne@afelio.be>
2021-04-20 20:17:22 +0200

    rename report url constant

--- src/jvmMain/kotlin/dev/remylavergne/ktoggl/Constant.kt
+++ src/jvmMain/kotlin/dev/remylavergne/ktoggl/Constant.kt
@@ -1,7 +1,7 @@
 package dev.remylavergne.ktoggl
 
 
-const val BASE_URL = "https://api.track.toggl.com/reports/api/v2"
+const val REPORT_BASE_URL = "https://api.track.toggl.com/reports/api/v2"
 const val API_KEY = ""
 const val USER_AGENT = ""
 const val WORKSPACE_ID = ""
